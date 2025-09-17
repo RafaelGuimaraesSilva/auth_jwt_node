a37c1427dde40b3e86b7dfdfe4a57ebb86fabfb8
Rafael Silva <rafaelguimaraesgv@hotmail.com>
2025-09-17 16:49:58 -0300

    Initial commit

---
+++ app.js
@@ -0,0 +1,42 @@
+//imports
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const app = express();
+
+// config JSON response
+
+app.use(express.json())
+
+// rota publica
+app.get('/', (req, res) => {
+    res.status(200).json({msg: 'Bem vindo a API'});
+});
+
+//register user
+app.post('/auth/register', async (req, res) => {
+    const {name, email, password, confirmpassword} = req.body
+
+    //validations
+    if(!name) {
+        return res.status(422).json({msg: 'O nome é obrigatório!'})
+    }
+})
+
+// credenciais
+
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+
+
+mongoose   
+    .connect(`mongodb+srv://${dbUser}:${dbPass}@cluster0.vb9dsk0.mongodb.net/`)
+    .then(() => {
+        app.listen(3000)
+        console.log('Conectou ao MongoDB')
+    })
+    .catch((err) => console.log(err))
+
